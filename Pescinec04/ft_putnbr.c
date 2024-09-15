b2ae6480fba20f87a112201017c1980841c35c6f
João Pedro Cunha e Rema <joaopcrema@gmail.com>
2024-09-15 14:54:44 +0100

    Add files via upload

---
+++ Pescinec04/ft_putnbr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 12:23:34 by jpedro-c          #+#    #+#             */
+/*   Updated: 2024/09/14 12:51:01 by jpedro-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb == -2147483648)
+	{
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+/*
+int main(void)
+{
+    ft_putnbr(9999);
+    ft_putchar('\n');
+    ft_putnbr(-2147483648);
+    ft_putchar('\n');
+    ft_putnbr(0);
+    ft_putchar('\n');
+    return 0;
+}
+*/
