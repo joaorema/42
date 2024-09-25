823a36ac92a7ed97da2da7322eb08ba37595ba6d
João Pedro Cunha e Rema <joaopcrema@gmail.com>
2024-09-25 18:36:25 +0100

    Add files via upload

--- Examexercises/rev_print.c
+++ Examexercises/rev_print.c
@@ -1,23 +1,20 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int	main(int ac, char *av[])
 {
-	int i;
-	
-	if(argc == 2)
+	int	i;
+	i = 0;
+	if (ac == 2)
 	{
-		i = 0;
-		while(argv[1][i])
+		while (av[1][i])
 		{
-			i++;
+			i += 1;
 		}
-		while (i >= 0)
+		while (i)
 		{
-			write(1, &argv[1][i], 1);
-			i--;
+			write(1, &av[1][--i], 1);
 		}
 	}
-	write (1, "\n", 1);
-	return 0;
+	write(1, "\n", 1);
+	return (0);
 }
-
